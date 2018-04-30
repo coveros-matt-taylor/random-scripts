f216dbb7d97515ff8502b23cbfb1dfefe7533612
Matt Taylor <mtaylor@sbd2.com>
2018-04-30 16:39:52 -0400

    divmod

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ with open('google-10000-english-usa-no-swears-medium.txt') as f:
 
 
 length = len(wordlist)
-pplength = 2
+pplength = 5
 quantity = 5
 
 print("number of words: {}".format(length))
@@ -24,14 +24,29 @@ for j in range(quantity):
         pp += str(randbelow(10))
     print(pp)
 
-perms = length ** pplength + (8 ** 2) + (9 ** 2)
+sep_quantity = (pplength-1)
+# sep_quantity = 2
+
+perms = length ** pplength + (8 ** sep_quantity) + (9 ** sep_quantity)
 entropy = ln(perms)
 
 perms2 = 26 ** 3 + 9 ** 5
 entropy2 = ln(perms2)
-print("passphrase possible permutations are {}".format(perms))
-print("entropy of passphrase is {}".format(entropy))
-print("password possible permutations are {}".format(perms2))
+print("passphrase possible permutations are {:,}".format(perms))
+print("entropy of passphrase is {:.6}".format(entropy))
+print("password possible permutations are {:,}".format(perms2))
 print("entropy of password is {}".format(entropy2))
 
+guesses = 100000000 # guesses per second
+s = perms // guesses
+m, s = divmod(s, 60)
+# s_rm = s % 60
+hr, m = divmod(m, 60)
+# m_rm = m % 60
+d, hr = divmod(hr, 24)
+# hr_rm = hr % 24
+yr, d = divmod(d, 365)
+
+cracked = "{:,d} years, {:d} days, {:d} hours, {:d} minutes, and {:d} seconds".format(yr, d, hr, m, s)
+print("At {:,} guesses per second,\nit would take {} to crack".format(guesses, cracked))
 
